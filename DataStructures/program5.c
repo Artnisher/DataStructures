de35d9a8db84c0755fab3a20d06fbe354a71fd1d
Artnisher <art191011@outlook.com>
2025-11-02 23:55:15 -0600

    program 5 completed

--- DataStructures/program5.c
+++ DataStructures/program5.c
@@ -1,6 +1,6 @@
 /**********************************************************************/
 /*                                                                    */
-/* Program Name: program5 - Recursively calculate even sum in a range inclusively      */
+/* Program Name: program5 - Recursively calculate even sum in a range */
 /* Author:       Arturo Chira                                         */
 /* Installation: Pensacola Christian College, Pensacola, Florida      */
 /* Course:       CS227, Data Structures and Algorithms                */
@@ -35,7 +35,7 @@
 /**********************************************************************/
 
 #include <stdio.h>    /* printf, scanf                                */
-#include <ctype.h>    /* toupper                                      */
+#include <ctype.h>    /* tolower, toupper                             */
 
 
 /**********************************************************************/
@@ -44,11 +44,10 @@
 #define COURSE_NUMBER     "CS227" /* PCC assigned course number       */
 #define PROGRAM_NUMBER    "5"     /* Teacher asigned program number   */
 #define LAST_NAME         "Chira" /* The programer's last name        */
-#define DATA_ALLOC_ERR    1       /* Error code if malloc fails       */
-#define MAX_VALUE         5000    /* Maximum number of customers     */
-#define MIN_VALUE        -5000    /* Minimum number of customers   */
-#define POSITIVE_ANSWER   'y'     /* Maximum length of a name         */
-#define NEGATIVE_ANSWER   'n'     /* Minimum length of a name        */
+#define MAX_VALUE         5000    /* Maximum amount accepted          */
+#define MIN_VALUE        -5000    /* Minimum amount accepted          */
+#define POSITIVE_ANSWER   'y'     /* proceed with calculation         */
+#define NEGATIVE_ANSWER   'n'     /* terminate the program            */
 
 
 /**********************************************************************/
@@ -66,8 +65,8 @@ int  swap_numbers(int *p_start_range, int *p_end_range);
   /* Swap the start and end range if needed                           */
 long even_numbers(int p_start_range, int p_end_range);
   /* Print even numbers in the range                                  */
-int  sum_even_numbers(int p_start_range, int p_end_range);
-  /* Calculate the sum of even numbers in the range                   */
+int  print_result(int p_start_range, int p_end_range);
+  /* Print the result of the even sum calculation                     */
 
 /**********************************************************************/
 /*                              Main Function                         */
@@ -77,18 +76,21 @@ int main()
   char answer;
   int p_start_range,
       p_end_range;
+
+   /* Print program heading and instructions                          */
    printf("\n\n\n\n\n\n");
    print_heading();                                     
    print_instructions();
+
    /* Processing a range of numbers until user quits                  */
    while ((answer = get_response()) == POSITIVE_ANSWER)
    {
        /* Get the start and end range from the user                   */
        get_range(&p_start_range , &p_end_range);
-       printf("\nProcessing the range %d to %d\n", p_start_range, p_end_range);
-       swap_numbers(&p_start_range, &p_end_range);
-        
-       sum_even_numbers(p_start_range, p_end_range);  
+       /* Swap the start and end range if needed                      */
+       swap_numbers(&p_start_range, &p_end_range); 
+       /* Calculate the sum of even numbers in the range              */
+       print_result(p_start_range, p_end_range);  
    }
   
    /* Print goodbye message before terminate the program              */
@@ -114,19 +116,19 @@ void print_heading()
 /**********************************************************************/
 void print_instructions()
 {
-   printf("\nThis program recursively calculates the sum of all\n");
-   printf("even whole numbers between two numbers inclusively.\n");
-   printf("You are repeatedly asked to enter in the start and\n");
-   printf("end of a range, and the sum of all even numbers in\n");
-   printf("that range is calculated and printed.  Information\n");
-   printf("about the recursive action of the function is also\n");
-   printf("printed so the recursive nature of the calculation\n");
-   printf("                 may be observed.\n"); 
+   printf("\nThis program recursively calculates the sum of all");
+   printf("\neven whole numbers between two numbers inclusively.");
+   printf("\nYou are repeatedly asked to enter in the start and");
+   printf("\nend of a range, and the sum of all even numbers in");
+   printf("\nthat range is calculated and printed.  Information");
+   printf("\nabout the recursive action of the function is also");
+   printf("\nprinted so the recursive nature of the calculation");
+   printf("\n               may be observed."); 
    return;
 }
 
 /**********************************************************************/
-/*                          Get Response                              */
+/*                     Get User Response                              */
 /**********************************************************************/
 char get_response()
 {
@@ -136,7 +138,7 @@ char get_response()
    do
    {
        printf("\nDo you want to process another range (y or n): ");
-       scanf (" %1s", &answer[0]);
+       scanf (" %c", &answer[0]);
        answer[0] = tolower(answer[0]);
    }
    while (answer[0] != POSITIVE_ANSWER && answer[0] != NEGATIVE_ANSWER);
@@ -145,15 +147,15 @@ char get_response()
 }
 
 /**********************************************************************/
-/*                          Get Range                                 */
+/*                     Get Range of Numbers                           */
 /**********************************************************************/
 void get_range(int *p_start_range, int *p_end_range)
 {
-  printf("\n");
+    printf("\n");
     do
     {
-        printf("Enter in the first number (from %d to %d): ", 
-               MIN_VALUE, MAX_VALUE);
+        printf("    Enter in the first number (from %d to %d): ", 
+                MIN_VALUE, MAX_VALUE);
         scanf ("%d", p_start_range);
     }
     while (*p_start_range < MIN_VALUE || *p_start_range > MAX_VALUE);
@@ -161,16 +163,19 @@ void get_range(int *p_start_range, int *p_end_range)
     printf("\n");
     do
     {
-      printf("Enter in the last number (from %d to %d):", 
-               MIN_VALUE, MAX_VALUE);
+        printf("    Enter in the last number (from %d to %d): ", 
+                MIN_VALUE, MAX_VALUE);
         scanf ("%d", p_end_range);
     }
     while (*p_end_range   < MIN_VALUE || *p_end_range   > MAX_VALUE);
+
+    printf("\nProcessing the range %d to %d: \n", *p_start_range, 
+           *p_end_range);
     
     return;
 }
 /**********************************************************************/
-/*                          Swap Range                                */
+/*                     Swap Numbers if Needed                        */
 /**********************************************************************/
 int swap_numbers(int *p_start_range, int *p_end_range)
 {
@@ -184,45 +189,49 @@ int swap_numbers(int *p_start_range, int *p_end_range)
 }
 
 /**********************************************************************/
-/*                         even numbers                               */
+/*                         Even numbers                               */
 /**********************************************************************/
 long even_numbers(int p_start_range, int p_end_range)
 {
     long  result;
-
+    
     printf("    Entering sum function for range %d to %d\n", 
-                                            p_start_range, p_end_range);
+            p_start_range, p_end_range);
+
     if(p_start_range > p_end_range)
     {
-        printf("\n");
-       // printf("    Exiting sum function for range %d to %d\n", 
-          //                                  p_start_range, p_end_range);
+        printf("    Exiting sum function for range "); 
+        printf("%d to %d with result: %ld\n", p_start_range, p_end_range, 
+               result);
         return 0;
     }
     
     if(p_start_range % 2 == 0)
     {
-        printf("      Adding: %d\n", p_start_range);
-        result = (long)p_start_range + even_numbers(p_start_range + 1, p_end_range);
+        printf("        Adding: %d\n", p_start_range);
+        result = (long)p_start_range + even_numbers(p_start_range + 1, 
+                 p_end_range);
     }
     else
     {
-        printf("      Skipping: %d\n", p_start_range);
-        return even_numbers(p_start_range + 1, p_end_range);
+        printf("        Skipping: %d\n", p_start_range);
+        result = even_numbers(p_start_range + 1, p_end_range);
     }
 
-    printf(" Exiting sum function for range %d to %d with result: %ld\n", 
-                                            p_start_range, p_end_range, result);
+    printf("    Exiting sum function for range ");
+    printf("%d to %d with result: %ld\n", p_start_range, p_end_range, 
+           result);
 
     return result;
 
 }
 /**********************************************************************/
-/*                     Sum even numbers                               */
+/*                         Print Result                               */
 /**********************************************************************/
-int sum_even_numbers(int p_start_range, int p_end_range)
+int print_result(int p_start_range, int p_end_range)
 {
     long total = even_numbers(p_start_range, p_end_range);
-    printf("\n The sum of even numbers is: %ld\n", total);
+    printf("The sum of all even numbers in the range %d to %d is: %ld\n"
+           ,p_start_range, p_end_range, total);
     return 0;
 }
